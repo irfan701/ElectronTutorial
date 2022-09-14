9a88715dcc7d12e51c4c8926ed430d7387730d2d
irfan701 <irfanhossain701@gmail.com>
2022-09-14 22:50:44 +0400

    Test Browser And Windows Properties
    Test Browser And Windows Properties

--- electron-app/main.js
+++ electron-app/main.js
@@ -1,13 +1,23 @@
 //console.warn("Electron In running!")
 
 const {app,BrowserWindow}=require('electron')
+console.warn("Main Process")
 
 function createWindow(){
    const win= new BrowserWindow({
       height:600,
-      width:900  
+      width:900,
+     // frame:true,
+      backgroundColor:"green",
+     // alwaysOnTop:false
+     title:"Awesome App"
     });
+
+    ///////////////////////////////////
     win.loadFile('index.html')
+    win.webContents.openDevTools()
 }
 
 app.whenReady().then(createWindow)
+
+////////////////////////////////////////
